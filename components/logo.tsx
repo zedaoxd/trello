ae348994b759d9d426001bd76174bceeef119762
Bruno Lessa <brunolessa@live.com>
2023-11-26 19:02:02 -0300

    feat: create Marketing Page

---
+++ components/logo.tsx
@@ -0,0 +1,22 @@
+import { cn } from "@/lib/utils";
+import localFont from "next/font/local";
+import Image from "next/image";
+import Link, { LinkProps } from "next/link";
+
+const headingFont = localFont({
+  src: "../public/fonts/font.woff2",
+});
+
+export const Logo = (linkProps: LinkProps) => {
+  return (
+    <Link {...linkProps}>
+      <div className="hover:opacity-75 transition items-center gap-x-2 hidden md:flex">
+        <Image src="/images/logo.svg" alt="logo" height={30} width={30} />
+
+        <p className={cn("text-lg text-neutral-700", headingFont.className)}>
+          Taskify
+        </p>
+      </div>
+    </Link>
+  );
+};
