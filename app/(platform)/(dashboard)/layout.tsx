1d74b159c8eeec639fa51072a66369f713160065
Bruno Lessa <brunolessa@live.com>
2023-11-26 23:13:32 -0300

    feat: switcher organizations

---
+++ app/(platform)/(dashboard)/layout.tsx
@@ -0,0 +1,15 @@
+import { Navbar } from "./_components/navbar";
+
+export default function DashboardLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <div className="h-full">
+      <Navbar />
+
+      {children}
+    </div>
+  );
+}
