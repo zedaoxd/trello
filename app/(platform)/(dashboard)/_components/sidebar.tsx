b7724542171fbda3d6284061ffe7453b6b096a5f
Bruno Lessa <brunolessa@live.com>
2023-11-28 00:17:37 -0300

    feat: create skeleton sidebar

--- app/(platform)/(dashboard)/_components/sidebar.tsx
+++ app/(platform)/(dashboard)/_components/sidebar.tsx
@@ -44,7 +44,16 @@ export const Sidebar = ({ storageKey = "t-sidebar-state" }: Props) => {
   if (!isLoadedOrg || !isLoadedOrgList || userMemberships.isLoading) {
     return (
       <>
-        <Skeleton />
+        <div className="flex items-center justify-between mb-2">
+          <Skeleton className="h-10 w-6/12" />
+          <Skeleton className="h-10 w-10" />
+        </div>
+
+        <div className="space-y-2">
+          <NavItem.Skeleton />
+          <NavItem.Skeleton />
+          <NavItem.Skeleton />
+        </div>
       </>
     );
   }
